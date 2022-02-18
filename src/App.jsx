0ecf356721ed260252b3877ce935da5653da0805
alejandropascuale <apascuale@outlook.com>
2022-02-18 10:09:59 -0300

    add all components to render in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css';
+import Contact from './components/contact/Contact';
+import Intro from './components/intro/Intro';
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonials from './components/testimonials/Testimonials';
 import TopBar from './components/topbar/TopBar';
+import Works from './components/works/Works';
 
 function App() {
   return (
     <div className="App">
       <TopBar />
-      
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
     </div>
   );
 }
