8cd3c45e905b56362de6761e4bb70ff8bd568268
alejandropascuale <apascuale@outlook.com>
2022-02-18 10:51:16 -0300

    add basic position of all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.scss';
 import Contact from './components/contact/Contact';
 import Intro from './components/intro/Intro';
 import Portfolio from './components/portfolio/Portfolio';
