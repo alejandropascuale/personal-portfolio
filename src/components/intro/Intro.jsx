7f5491708686e2147fee7a326505459f259ef084
alejandropascuale <apascuale@outlook.com>
2022-02-18 15:12:38 -0300

    create welcome text in home

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -8,7 +8,16 @@ export default function Intro() {
           <img src="assets/man.png" alt="" />
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Alejandro Pascuale</h1>
+          <h3>Full Stack Developer <span>Designer</span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
