d91283ffb8217b557e74aa5416040561823a670e
alejandropascuale <apascuale@outlook.com>
2022-02-18 11:29:54 -0300

    add link to intro section in topbar

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -3,7 +3,14 @@ import './topbar.scss'
 export default function TopBar() {
   return (
     <div className='topbar'>
-      
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro">intro</a>
+        </div>
+        <div className="right">
+
+        </div>
+      </div>
     </div>
   )
 }
