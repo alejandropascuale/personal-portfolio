014de925f9692115dfc9c37298c701ac057b6d99
alejandropascuale <apascuale@outlook.com>
2022-02-21 09:16:04 -0300

    create PortfolioList component to render list of technologies

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,15 +1,45 @@
+import React, { useState, useEffect } from 'react'
+
+import PortfolioList from '../portfolioList/PortfolioList'
 import './portfolio.scss'
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState('featured');
+  const featuredList = [
+    {
+      id: 'featured',
+      title: 'Featured'
+    },
+    {
+      id: 'webApi',
+      title: 'Web Api'
+    },
+    {
+      id: 'mobileApp',
+      title: 'Mobile App'
+    },
+    {
+      id: 'design',
+      title: 'Design'
+    },
+    {
+      id: 'branding',
+      title: 'Branding'
+    }
+  ]
   return (
     <div className='portfolio' id='portfolio'>
       <h1>Portfolio</h1>
       <ul>
-        <li className='active'>Featured</li>
-        <li>Web Api</li>
-        <li>Mobile App</li>
-        <li>Design</li>
-        <li>Branding</li>
+        {featuredList.map(item => (
+          <PortfolioList 
+            title={item.title} 
+            key={item.id} 
+            active={selected === item.id}
+            setSelected={setSelected} 
+            id={item.id}
+          />
+        ))}
       </ul>
       <div className="container">
         <div className="item">
